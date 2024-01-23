60877c6eed5d519261bb729f53a8f626c495cdd4
stonkmem <stonkeroctober@gmail.com>
2024-01-23 16:16:30 +0800

    Copy Paste TF2

--- client/sketch.js
+++ client/sketch.js
@@ -1,7 +1,9 @@
 let ball;
 
+const socket = io.connect("ws://localhost:8000");
+let em = EntityManager();
 function setup() {
-	new Canvas(500, 500);
+	new Canvas("fullscreen");
 
 	ball = new Sprite();
 	ball.diameter = 50;
@@ -9,4 +11,33 @@ function setup() {
 
 function draw() {
 	background('grey');
+	move();
+	socket.emit("position", ball.pos.x, ball.pos.y);
 }
+
+function move() {
+    const SPEED = 10;
+    if (kb.pressing("w")) {
+        ball.pos.y -= SPEED;
+    }
+    if (kb.pressing("a")) {
+        ball.pos.x -= SPEED;
+    }
+    if (kb.pressing("s")) {
+        ball.pos.y += SPEED;
+    }
+    if (kb.pressing("d")) {
+        ball.pos.x += SPEED;
+    }
+}
+socket.on("playerDataUpdate", (id, playerData) => {
+    for (let data of playerData) {
+        if (data.id === id)
+            continue;
+        if (!em.exists(data.id)) {
+            em.registerNewPlayer(data);
+        } else {
+            em.updatePlayerData(data);
+        }
+    }
+});
